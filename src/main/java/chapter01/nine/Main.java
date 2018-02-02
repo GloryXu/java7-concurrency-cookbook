adfbdcebf4cf2142b8f0a48ded89b8ba2c928a3a
xugr <xugr>
2018-02-02 11:39:47 +0800

    Thread 添加异常捕获setUncaughtExceptionHandler

---
+++ src/main/java/chapter01/nine/Main.java
@@ -0,0 +1,10 @@
+package chapter01.nine;
+
+public class Main {
+    public static void main(String[] args) {
+        Task task = new Task();
+        Thread thread = new Thread(task);
+        thread.setUncaughtExceptionHandler(new ExceptionHandler());
+        thread.start();
+    }
+}
---
+++ src/main/java/chapter01/nine/Task.java
@@ -0,0 +1,9 @@
+package chapter01.nine;
+
+public class Task implements Runnable {
+
+    @Override
+    public void run() {
+        int numero = Integer.parseInt("TTT");
+    }
+}
