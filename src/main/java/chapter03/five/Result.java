6342ffb6330d2e6718ddf58f8c09cb04d36df486
xugr <xugr>
2018-02-12 11:18:33 +0800

    在集合点的同步

---
+++ src/main/java/chapter03/five/Result.java
@@ -0,0 +1,17 @@
+package chapter03.five;
+
+public class Result {
+    private int data[];
+
+    public Result(int size) {
+        data = new int[size];
+    }
+
+    public void setData(int position, int value) {
+        data[position] = value;
+    }
+
+    public int[] getData() {
+        return data;
+    }
+}
