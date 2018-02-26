b8c265dbe41ed1320e94488b6c19828c2193b55f
xugr <xugr>
2018-02-26 19:45:38 +0800

    创建线程执行器，executorService

---
+++ src/main/java/chapter04/two/Main.java
@@ -0,0 +1,12 @@
+package chapter04.two;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+        for(int i = 0;i<50;i++) {
+            Task task = new Task("Task " + i);
+            server.executeTask(task);
+        }
+        server.endServer();
+    }
+}
